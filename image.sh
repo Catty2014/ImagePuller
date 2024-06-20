dbc4251e1f1451f65db2072bec0437bb7500221e
CattySteve <cattysteve89265@163.com>
2024-06-20 11:18:17 +0800

    20240620-vulhub-with-pullargs

--- image.sh
+++ image.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 docker login -u github -p $1 docker.cattysteve.top
+export a=$(cat pullargs.txt)
 for i in $(cat images.txt)
 do
-    docker pull $i
+    echo "Pulling $i using $a"
+    docker pull $i $a
     docker tag $i docker.cattysteve.top/mirror_gh/$i
     docker push docker.cattysteve.top/mirror_gh/$i
 done
